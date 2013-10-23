ff2c3e148cd2c44f0387f43485487c77ec294aa4
Avishek <avisheks@mindfiresolutions.com>
2013-10-23 19:56:17 +0530

    we dont bother for output, pass no parameters to run function

--- index.php
+++ index.php
@@ -1,26 +1,18 @@
 <?php
-/*
-$output = shell_exec('sleep 5 > /dev/null 2>&1 & echo $!');
-echo "<pre>$output</pre>";
-exit;
-
-*/
 require_once("BGClass.php");
 $process = new BackgroundProcess();
 $process->setCommand('/usr/bin/php bgprocess.php');
-$process->run('/var/www/bgprocess/bgprocess.out','/var/www/bgprocess/errorlog.out');
-echo "<input type='text' id='pid' value=".$process->getPid().">";
-/*
-echo sprintf('Crunching numbers in process %d', $process->getPid());
-while ($process->isRunning()) {
-    echo '.';
-    sleep(1);
-}
-echo "\nDone.\n";
-*/
-
+//if you want out or error log info to some file path
+//$process->run('/var/www/bgprocess/bgprocess.out','/var/www/bgprocess/errorlog.out');
+//if you dont bother for error or output
+$process->run();
+echo "<center>
+	<h1>Back Ground Process</h1><hr>
+	<label>Process Id:</label>
+	<input type='text' id='pid' value=".$process->getPid().">
+      </center>";
 ?>
-<a id="notification" href="#">get notification</a>
+<center><a id="notification" href="#">get notification</a></center>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
 </script>
 <script>
